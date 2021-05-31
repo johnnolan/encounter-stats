946bab23ecf2a627359f770f7e17bd0c7d93d97b
John Nolan <me@johnnolan.dev>
2021-05-31 18:13:38 +0100

    Disabled tests until I use testing-library method

--- scripts/Template.test.bu.js
+++ scripts/Template.test.bu.js
@@ -12,6 +12,7 @@ beforeAll(() => {
         id: "WTi2ws6xbFXJBhAb",
         img: "/test/img.png",
         hp: "13",
+        type: "character",
         max: "16",
         ac: "11",
         events: [],
@@ -23,6 +24,11 @@ beforeAll(() => {
         },
       },
     ],
+    top: {
+      maxDamage: "Lorena Aldabra<br />57",
+      highestAvgDamage: "Graa S'oua<br />49",
+      highestMaxDamage: "Graa S'oua<br />49",
+    }
   };
   statsData = {
     encounterId: "sze0mlf69xCj913F",
@@ -32,6 +38,7 @@ beforeAll(() => {
         id: "WTi2ws6xbFXJBhAb",
         img: "/test/img.png",
         hp: "13",
+        type: "character",
         max: "16",
         ac: "11",
         events: [
@@ -61,76 +68,101 @@ beforeAll(() => {
         },
       },
     ],
+    top: {
+      maxDamage: "Lorena Aldabra<br />57",
+      highestAvgDamage: "Graa S'oua<br />49",
+      highestMaxDamage: "Graa S'oua<br />49",
+    },
   };
 });
 
 describe("On generating the markup from an object with no events", () => {
   test("Expect the HTML to be valid", () => {
     const markup = Generate(statsDataWithNoEvents);
+    console.log(markup)
     expect(markup).toEqual(`
-  <div class="fvtt-enc-stats">
-    <h1>
-        Encounter ID: sze0mlf69xCj913F
-    </h1>
+    <div class="fvtt-enc-stats">
     <hr />
-    <div class="fvtt-enc-stats_combatants">
-      <hr />
-  <div class="fvtt-enc-stats_combatant" data-fvtt-id="WTi2ws6xbFXJBhAb">
-    <h2>Graa</h2>
-    <div class="fvtt-enc-stats_combatants_overview">
-      <div class="fvtt-enc-stats_combatants_actor">
-        <table>
-          <tbody>
-            <tr>
-              <th width="120px" scope="col"></th>
-              <th scope="col">HP</th>
-              <th scope="col">Max HP</th>
-              <th scope="col">AC</th>
-            </tr>
-            <tr>
-              <td class="fvtt-enc-stats_combatants_actor_image"><img src="/test/img.png" width="100" height="100" alt="Graa" /></td>
-              <td>13</td>
-              <td>16</td>
-              <td>11</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="fvtt-enc-stats_combatants_attacks">
-        <table>
-          <tbody data-fvtt-attack-id="WTi2ws6xbFXJBhAb">
-            <tr>
-              <td></td>
-              <th scope="col">Weapon Name</th>
-              <th scope="col">Advantage</th>
-              <th scope="col">Disadvantage</th>
-              <th scope="col">Attack Total</th>
-              <th scope="col">Damage Total</th>
-            </tr>
-          </tbody>
-        </table>
+    <div class="fvtt-enc-stats_top">
+      <div class="fvtt-enc-stats_actor_statlist flexrow">
+        <div class="fvtt-enc-stats_actor_stat">
+          <div class="fvtt-enc-stats_actor_stat-key">Most Damage Overall</div>
+          <div class="fvtt-enc-stats_actor_stat-value">Lorena Aldabra<br />57</div>
+        </div>
+        <div class="fvtt-enc-stats_actor_stat">
+          <div class="fvtt-enc-stats_actor_stat-key">Highest Average Damage</div>
+          <div class="fvtt-enc-stats_actor_stat-value">Graa S'oua<br />49</div>
+        </div>
+        <div class="fvtt-enc-stats_actor_stat">
+          <div class="fvtt-enc-stats_actor_stat-key">Highest Damage in 1 hit</div>
+          <div class="fvtt-enc-stats_actor_stat-value">Graa S'oua<br />49</div>
+        </div>
       </div>
     </div>
-    <div class="fvtt-enc-stats_combatants_summary">
-      <table>
-        <tbody class="fvtt-enc-stats_combatants_summary-table" data-fvtt-attack-summary-id="WTi2ws6xbFXJBhAb">
-          <tr>
-            <th scope="col">Min</th>
-            <th scope="col">Max</th>
-            <th scope="col">Average</th>
-            <th scope="col">Damage Total</th>
-          </tr>
-          <tr>
-            <td>18</td>
-            <td>18</td>
-            <td>18</td>
-            <td>18</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="fvtt-enc-stats_combatants">
+    <div>
+  <div class="fvtt-enc-stats_combatant" data-fvtt-id="WTi2ws6xbFXJBhAb">
+    <div class="fvtt-enc-stats_combatants_overview">
+      <header class="fvtt-enc-stats_combatants_actor flexrow">
+        <div class="fvtt-enc-stats_combatants_actor_image flexcol">
+          <img src="/test/img.png" alt="Graa" />
+        </div>
+        <div class="fvtt-enc-stats_actor_stats">
+          <h1 class="fvtt-enc-stats_actor_stats_name">Graa</h1>
+          <div class="fvtt-enc-stats_actor_statlist flexrow">
+            <div class="fvtt-enc-stats_actor_stat">
+              <div class="fvtt-enc-stats_actor_stat-key">HP</div>
+              <div class="fvtt-enc-stats_actor_stat-value">
+                <span>13</span><span class="sep">/</span><span>16</span></div>
+            </div>
+            <div class="fvtt-enc-stats_actor_stat">
+              <div class="fvtt-enc-stats_actor_stat-key">AC</div>
+              <div class="fvtt-enc-stats_actor_stat-value"><span>11</span></div>
+            </div>
+            <div class="fvtt-enc-stats_actor_stat">
+              <div class="fvtt-enc-stats_actor_stat-key">Damage Total</div>
+              <div class="fvtt-enc-stats_actor_stat-value"><span>18</span></div>
+            </div>
+            <div class="fvtt-enc-stats_actor_stat">
+              <div class="fvtt-enc-stats_actor_stat-key">Min Damage</div>
+              <div class="fvtt-enc-stats_actor_stat-value"><span>18</span></div>
+            </div>
+            <div class="fvtt-enc-stats_actor_stat">
+              <div class="fvtt-enc-stats_actor_stat-key">Max Damage</div>
+              <div class="fvtt-enc-stats_actor_stat-value"><span>18</span></div>
+            </div>
+            <div class="fvtt-enc-stats_actor_stat">
+              <div class="fvtt-enc-stats_actor_stat-key">Avg Damage</div>
+              <div class="fvtt-enc-stats_actor_stat-value"><span>18</span></div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section class="fvtt-enc-stats_combatants_attacks">
+        <div class="flexcol">
+          <ol class="items-list flexcol">
+            <li class="items-header flexrow">
+              <div class="item-name item-round">Round</div>
+              <div class="item-name item-weapon">Weapon/Spell Name</div>
+              <div class="item-name">Advantage</div>
+              <div class="item-name">Disadvantage</div>
+              <div class="item-name">Attack Total</div>
+              <div class="item-name">Damage Total</div>
+            </li>
+            <ol class="item-list">
+
+
+            </ol>
+          </ol>
+        </div>
+      </section>
     </div>
   </div>
-  </div></div>
+  </div>
+      <h2 class="fvtt-enc-stats_enemies">
+          Enemies
+      </h2>
+      <div></div></div></div>
   `);
   });
 });
