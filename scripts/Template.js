3eaf2591a80b7a057a727037f4c32d255dbd8430
John Nolan <me@johnnolan.dev>
2021-06-12 07:50:27 +0100

    Adds data to template

--- scripts/Template.js
+++ scripts/Template.js
@@ -114,42 +114,66 @@ function GenerateCombatant(combatant) {
           </div>
         </div>
       </header>
-      <section class="fvtt-enc-stats_combatants_attacks">
-        <div class="flexcol">
-          <ol class="items-list flexcol">
-            <li class="items-header flexrow">
-              <div class="item-name item-round">${game.i18n.format(
-                "FVTTEncounterStats.template.round"
-              )}</div>
-              <div class="item-name item-weapon">${game.i18n.format(
-                "FVTTEncounterStats.template.weapon_spell_name"
-              )}</div>
-              <div class="item-name">${game.i18n.format(
-                "FVTTEncounterStats.template.advantage"
-              )}</div>
-              <div class="item-name">${game.i18n.format(
-                "FVTTEncounterStats.template.disadvantage"
-              )}</div>
-              <div class="item-name">${game.i18n.format(
-                "FVTTEncounterStats.template.isCritical"
-              )}</div>
-              <div class="item-name">${game.i18n.format(
-                "FVTTEncounterStats.template.attack_total"
-              )}</div>
-              <div class="item-name">${game.i18n.format(
-                "FVTTEncounterStats.template.damage_total"
-              )}</div>
-            </li>
-            <ol class="item-list">
-              ${combatant.events
-                .map(function (event) {
-                  return GenerateAttackRow(event);
-                })
-                .join("")}
-                     
+      <section class="fvtt-enc-stats_combatants_data">
+        <section class="fvtt-enc-stats_combatants_health">
+          <div class="fvtt-enc-stats_title3">${game.i18n.format(
+            "FVTTEncounterStats.template.health"
+          )}</div>
+          <div class="flexcol">
+            <ol class="items-list flexcol">
+              <li class="items-header flexrow">
+                <div class="item-name item-round">${game.i18n.format(
+                  "FVTTEncounterStats.template.round"
+                )}</div>
+                <div class="item-name">${game.i18n.format(
+                  "FVTTEncounterStats.template.hp"
+                )}</div>
+              </li>
+              <ol class="item-list">
+                ${combatant.health
+                  .map(function (event) {
+                    return GenerateHealtRow(event);
+                  })
+                  .join("")}
+                      
+              </ol>
             </ol>
-          </ol>
-        </div>
+          </div>
+        </section>
+        <section class="fvtt-enc-stats_combatants_attacks">
+          <div class="fvtt-enc-stats_title3">${game.i18n.format(
+            "FVTTEncounterStats.template.attacks"
+          )}</div>
+          <div class="flexcol">
+            <ol class="items-list flexcol">
+              <li class="items-header flexrow">
+                <div class="item-name item-round">${game.i18n.format(
+                  "FVTTEncounterStats.template.round"
+                )}</div>
+                <div class="item-name item-weapon">${game.i18n.format(
+                  "FVTTEncounterStats.template.weapon_spell_name"
+                )}</div>
+                <div class="item-name">${game.i18n.format(
+                  "FVTTEncounterStats.template.rolltype"
+                )}</div>
+                <div class="item-name">${game.i18n.format(
+                  "FVTTEncounterStats.template.attack_total"
+                )}</div>
+                <div class="item-name">${game.i18n.format(
+                  "FVTTEncounterStats.template.damage_total"
+                )}</div>
+              </li>
+              <ol class="item-list">
+                ${combatant.events
+                  .map(function (event) {
+                    return GenerateAttackRow(event);
+                  })
+                  .join("")}
+                      
+              </ol>
+            </ol>
+          </div>
+        </section>
       </section>
     </div>
   </div>
@@ -165,11 +189,29 @@ function GenerateAttackRow(event) {
     <div class="item-name item-weapon">${
       event.item.itemLink ? event.item.itemLink : event.item.name
     }</div>
-    <div class="item-name">${event.advantage}</div>
-    <div class="item-name">${event.disadvantage}</div>
-    <div class="item-name">${event.isCritical}</div>
-    <div class="item-name">${event.attackTotal}</div>
+    <div class="item-name">${
+      event.advantage
+        ? "advantage"
+        : event.disadvantage
+        ? "disadvantage"
+        : "normal"
+    }</div>
+    <div class="item-name">${event.attackTotal} ${
+    event.isCritical ? " (c)" : ""
+  }</div>
     <div class="item-name">${event.damageTotal}</div>
+  </li>`;
+
+  return markup;
+}
+
+function GenerateHealtRow(event) {
+  let markup = `
+  <li class="item flexrow">
+    <div class="item-name item-round">${event.round}</div>
+    <div class="item-name">${event.current} (${event.isheal ? "+" : "-"}${
+    event.diff
+  })</div>
   </li>`;
 
   return markup;
