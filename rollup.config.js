03bea00a94a0947fd49ea1f42ac52b08b9fd6cec
John Nolan <me@johnnolan.dev>
2021-06-22 22:00:19 +0100

    Move files to use Rollup for adding cross browser compatibility

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/module.js',
+  output: {
+    file: 'scripts/module.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+    resolve(),
+    nodeResolve(),
+    babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
+    commonjs()
+  ]  
+};
