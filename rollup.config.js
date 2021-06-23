e128987a9d27ccb61ab0d3c063d16924cffde06c
John Nolan <me@johnnolan.dev>
2021-06-23 17:02:02 +0100

    Move css to scss

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,29 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import scss from "rollup-plugin-scss";
 
-export default {
-  input: "src/module.js",
-  output: {
-    file: "scripts/module.js",
-    format: "cjs",
-    sourcemap: true,
+export default [
+  {
+    input: "src/module.js",
+    output: {
+      file: "scripts/module.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      nodeResolve(),
+      babel({ babelHelpers: "bundled", exclude: "node_modules/**" }),
+    ],
   },
-  plugins: [
-    resolve(),
-    nodeResolve(),
-    babel({ babelHelpers: "bundled", exclude: "node_modules/**" }),
-  ],
-};
+  {
+    input: "src/scss.js",
+    output: {
+      file: "styles/module.js",
+      format: "esm",
+      sourcemap: false,
+    },
+    plugins: [scss()],
+  },
+];
