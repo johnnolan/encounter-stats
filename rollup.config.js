5040a9922c4e5120bf72ffa234740f4f9e66e31b
John Nolan <me@johnnolan.dev>
2021-06-27 19:39:20 +0100

    Revert babel compilation changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,6 @@
-import resolve from "@rollup/plugin-node-resolve";
-import babel from "@rollup/plugin-babel";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import scss from "rollup-plugin-scss";
 
 export default [
-  {
-    input: "src/module.js",
-    output: {
-      file: "scripts/module.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      nodeResolve(),
-      babel({ babelHelpers: "bundled", exclude: "node_modules/**" }),
-    ],
-  },
   {
     input: "src/scss.js",
     output: {
