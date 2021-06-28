d4f1a9528431861069ce35f750b0e264b44ea009
John Nolan <me@johnnolan.dev>
2021-06-28 09:29:26 +0100

    Only require the minimum info from the debug data

--- scripts/panels/ConfigPanel.js
+++ scripts/panels/ConfigPanel.js
@@ -10,10 +10,39 @@ export class ConfigPanel extends FormApplication {
     });
   }
   getData() {
+    var moduleData = [];
+    for (const [key, value] of game.modules.entries()) {
+      moduleData.push({
+        id: value.id,
+        name: value.data.name,
+        active: value.active,
+        compatibleCoreVersion: value.data.compatibleCoreVersion,
+        minimumCoreVersion: value.data.minimumCoreVersion,
+        version: value.data.version,
+      });
+    }
+    var gameSettingsData = [];
+    for (const [key, value] of game.settings.settings.entries()) {
+      if (
+        key.indexOf("fvtt-encounter-stats.") === 0 ||
+        key.indexOf("midi-qol.") === 0 ||
+        key.indexOf("betterrolls5e.") === 0 ||
+        key.indexOf("beyond20.") === 0
+      ) {
+        let keySplit = key.split(".");
+        let gameSetting = game.settings.get(`${keySplit[0]}`, `${keySplit[1]}`);
+        gameSettingsData.push({
+          id: key,
+          value: gameSetting,
+        });
+      }
+    }
     let data = {
-      debug: `World Core Version: ${
-        game.world.data.coreVersion
-      }. Modules: ${JSON.stringify(Object.fromEntries(game.modules))}`,
+      debug: JSON.stringify({
+        coreVersion: game.world.data.coreVersion,
+        modules: moduleData,
+        settings: gameSettingsData,
+      }),
     };
     return data;
   }
