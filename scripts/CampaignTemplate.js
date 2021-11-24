38cb57bcbf99b777c3f3796c393cfb2227d73c96
John Nolan <me@johnnolan.dev>
2021-11-24 19:38:36 +0000

    asd

--- scripts/CampaignTemplate.js
+++ scripts/CampaignTemplate.js
@@ -124,7 +124,11 @@ function _getItemListTitle(key, event, simpleCalendarEnabled) {
   let markup = ``;
 
   if (simpleCalendarEnabled) {
-    if (event[key] && event[key].length > 0 && event[key].simpleCalendarName) {
+    if (
+      event[key] &&
+      event[key].length > 0 &&
+      event[key][0].simpleCalendarName
+    ) {
       markup += `<h3>${event[key][0].simpleCalendarName}</h3><ol class="item-list">`;
     } else {
       markup += `<h3>${key}</h3><ol class="item-list">`;
