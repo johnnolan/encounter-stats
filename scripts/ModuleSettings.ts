58107bd2381cad6a9ea12bb5a415ff34980d5b61
John Nolan <me@johnnolan.dev>
2022-08-24 19:35:34 +0100

    Remove redundant code

--- scripts/ModuleSettings.ts
+++ scripts/ModuleSettings.ts
@@ -1,10 +1,4 @@
-import {
-  MODULE_ID,
-  OPT_ENABLE,
-  OPT_REPORT_BUG,
-  OPT_ENABLE_SIMPLE_CALENDAR_INTEGRATION,
-} from "./Settings";
-import { ConfigPanel } from "./panels/ConfigPanel";
+import { MODULE_ID, OPT_ENABLE } from "./Settings";
 import Trans from "./Trans";
 
 class ModuleSettings {
@@ -22,26 +16,6 @@ class ModuleSettings {
       default: true,
       type: Boolean,
     });
-    game.settings.registerMenu(`${MODULE_ID}`, `${OPT_REPORT_BUG}`, {
-      name: Trans.Get("config.debug"),
-      label: Trans.Get("config.debug"),
-      icon: "fas fa-bug",
-      scope: "world",
-      type: ConfigPanel,
-      restricted: true,
-    });
-    game.settings.register(
-      `${MODULE_ID}`,
-      `${OPT_ENABLE_SIMPLE_CALENDAR_INTEGRATION}`,
-      {
-        name: Trans.Get("opt_enable_simple_calendar_integration_name"),
-        hint: Trans.Get("opt_enable_simple_calendar_integration_hint"),
-        scope: "world",
-        config: true,
-        default: true,
-        type: Boolean,
-      }
-    );
   }
 }
 
