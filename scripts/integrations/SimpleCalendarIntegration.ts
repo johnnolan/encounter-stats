1bea0d4ef1d8852103b6ee3e4c57fb35dc2610e7
John Nolan <me@johnnolan.dev>
2022-09-06 09:10:46 +0100

    Add `foundryvtt-simple-calendar` npm library
    Simplify Simple Calendar integration code

--- scripts/integrations/SimpleCalendarIntegration.ts
+++ scripts/integrations/SimpleCalendarIntegration.ts
@@ -1,20 +1,13 @@
 export default class SimpleCalendarIntegration {
   static IsEnabled() {
-    return window.SimpleCalendar !== undefined;
-  }
-
-  private static GetCurrentDate() {
-    return {
-      day: window.SimpleCalendar.api.getCurrentDay().name,
-      month: window.SimpleCalendar.api.getCurrentMonth().name,
-      year: `${
-        window.SimpleCalendar.api.getCurrentYear().numericRepresentation
-      } ${window.SimpleCalendar.api.getCurrentYear().postfix}`,
-    };
+    return typeof SimpleCalendar !== "undefined";
   }
 
   static GetCurrentDateToString() {
-    const d = this.GetCurrentDate();
-    return `${d.day} ${d.month} ${d.year}`;
+    return `${SimpleCalendar.api.getCurrentDay()?.name} ${
+      SimpleCalendar.api?.getCurrentMonth()?.name
+    } ${SimpleCalendar.api?.getCurrentYear()?.numericRepresentation} ${
+      SimpleCalendar.api?.getCurrentYear()?.postfix
+    }`;
   }
 }
