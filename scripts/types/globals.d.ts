0ed8b590345b05462ae1801390f751048f81e39e
John Nolan <me@johnnolan.dev>
2022-10-30 11:39:32 +0000

    Extract MidiQolWorkflow interface

--- scripts/types/globals.d.ts
+++ scripts/types/globals.d.ts
@@ -56,66 +56,6 @@ declare global {
     isFumble: boolean;
   }
 
-  interface MidiQolWorkflow {
-    id: string;
-    actor: {
-      id: string;
-    };
-    item: MidiQolEventItem;
-    attackRoll: {
-      options: {
-        advantageMode: number;
-      };
-      total: number;
-    };
-    damageTotal: number;
-    attackTotal: number;
-    workflowType: string;
-    isCritical: boolean;
-    isFumble: boolean;
-    targets: Array<{
-      id: string;
-      sheet: {
-        actor: {
-          name: string;
-          system: {
-            attributes: {
-              ac: {
-                value: number;
-              };
-            };
-          };
-        };
-      };
-    }>;
-    hitTargets: Array<{
-      id: string;
-      sheet: {
-        actor: {
-          name: string;
-          system: {
-            attributes: {
-              ac: {
-                value: number;
-              };
-            };
-          };
-        };
-      };
-    }>;
-  }
-
-  interface MidiQolEventItem {
-    id: string;
-    name: string;
-    link: string;
-    type: string;
-    img: string;
-    system: {
-      actionType: string;
-    };
-  }
-
   interface EncounterWorkflow {
     id: string;
     actor: {
