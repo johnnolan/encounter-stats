ca52f8304a4100542c81c8118b6697174c843434
John Nolan <me@johnnolan.dev>
2022-10-30 11:10:53 +0000

    CampaignRenderer interface extraction

--- scripts/types/globals.d.ts
+++ scripts/types/globals.d.ts
@@ -21,46 +21,13 @@ declare global {
       };
     };
   }
+
   interface HookRenderCombatTrackerData {
     user: User;
     combats: Array<Combat>;
     combatCount: number;
     hasCombat: boolean;
     combat: Combat;
-  }
-
-  interface CampaignRender {
-    nat1: Array<CampaignRollData>;
-    nat20: Array<CampaignRollData>;
-    heals: Array<CampaignRollData>;
-    kills: Array<CampaignRollData>;
-    criticalHistory: Array<CampaignRollRowData>;
-    fumbleHistory: Array<CampaignRollRowData>;
-    healsHistory: Array<CampaignRollRowData>;
-    killsHistory: Array<CampaignRollRowData>;
-    customEventHistory: Array<CampaignCustomData>;
-  }
-
-  interface CampaignCustomData {
-    name: string;
-    events: Array<CampaignRollRowData>;
-  }
-
-  interface CampaignRollData {
-    name: string;
-    value: string;
-  }
-
-  interface CampaignRollRowData {
-    date: string;
-    entries: Array<CampaignRollRowDataItem>;
-  }
-
-  interface CampaignRollRowDataItem {
-    actorName: string;
-    flavor: string;
-    date: string;
-    total?: number;
   }
 
   interface CustomHookEvent {
