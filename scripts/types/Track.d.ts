59b4afdb63725d172bee8f6a48923f5d69f7107d
John Nolan <me@johnnolan.dev>
2022-10-30 11:19:02 +0000

    Track type interface extraction

---
+++ scripts/types/Track.d.ts
@@ -0,0 +1,19 @@
+interface DiceTrack {
+  actorName: string;
+  flavor: string;
+  date: string;
+}
+
+interface HealTrack {
+  actorName: string;
+  itemLink: string;
+  spellName: string;
+  total: number;
+  date: string;
+}
+
+interface KillTrack {
+  actorName: string;
+  tokenName: string;
+  date: string;
+}
--- scripts/types/globals.d.ts
+++ scripts/types/globals.d.ts
@@ -49,26 +49,6 @@ declare global {
     dateDisplay: string;
   }
 
-  interface DiceTrack {
-    actorName: string;
-    flavor: string;
-    date: string;
-  }
-
-  interface HealTrack {
-    actorName: string;
-    itemLink: string;
-    spellName: string;
-    total: number;
-    date: string;
-  }
-
-  interface KillTrack {
-    actorName: string;
-    tokenName: string;
-    date: string;
-  }
-
   interface DiceTrackParse {
     name: string;
     flavor: string;
