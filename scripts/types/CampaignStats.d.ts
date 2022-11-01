36596b793f57d3596e7a084c2f1746e46f69af49
John Nolan <me@johnnolan.dev>
2022-11-01 17:07:28 +0000

    Refactor

--- scripts/types/CampaignStats.d.ts
+++ scripts/types/CampaignStats.d.ts
@@ -1,8 +1,9 @@
-interface CustomDataEvent {
-  EventName: string;
-  FlavorText: string;
-  actorName?: string;
-  date: string;
+interface CampaignStats {
+  nat1: Array<CampaignStat>;
+  nat20: Array<CampaignStat>;
+  heals: Array<CampaignStat>;
+  kills: Array<CampaignStat>;
+  custom: Array<CampaignStat>;
 }
 
 interface CampaignStat {
@@ -11,16 +12,15 @@ interface CampaignStat {
   data: Array<DiceTrack> | Array<CustomDataEvent>;
 }
 
+interface CustomDataEvent {
+  EventName: string;
+  FlavorText: string;
+  actorName?: string;
+  date: string;
+}
+
 interface DiceTrack {
   actorName: string;
   flavor: string;
   date: string;
-}
-
-interface CampaignStats {
-  nat1: Array<CampaignStat>;
-  nat20: Array<CampaignStat>;
-  heals: Array<CampaignStat>;
-  kills: Array<CampaignStat>;
-  custom: Array<CampaignStat>;
 }
