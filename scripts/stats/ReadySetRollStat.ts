dd80b3afaa0d65a9b9bf6f76f27c29c4a4020dda
John Nolan <me@johnnolan.dev>
2023-01-20 08:32:23 +0000

    Handle manual damage role

--- scripts/stats/ReadySetRollStat.ts
+++ scripts/stats/ReadySetRollStat.ts
@@ -7,11 +7,14 @@ export default class ReadySetRollStat extends Stat {
     const stat = new Stat();
     stat.encounter = await StatManager.GetStat();
     const combatantStat = stat.GetCombatantStatsByTokenId(tokenId);
-    if (combatantStat.events.length > 0) {
+    if (combatantStat?.events?.length ?? 0 > 0) {
       const lastStat = combatantStat.events[combatantStat.events.length - 1];
       const lastDamageValue = lastStat.damageMultipleEnemiesTotal ?? 0;
-      lastStat.damageMultipleEnemiesTotal = lastDamageValue + damage;
+      lastStat.damageMultipleEnemiesTotal = lastDamageValue + Math.abs(damage);
     }
+
+    // TODO:
+    // 1: Not updating multiple times, race condition of the events?
 
     await stat.Save();
   }
@@ -33,35 +36,53 @@ export default class ReadySetRollStat extends Stat {
       );
       return;
     }
+    const lastCombatantEvent =
+      combatantStat.events[combatantStat.events.length - 1];
 
-    const newCombatantEvent = <CombatantEvent>{
-      id: workflow.id,
-      actorId: workflow.actor.id,
-      item: workflow.item,
-      advantage: workflow.advantage,
-      disadvantage: workflow.disadvantage,
-      actionType: workflow.actionType,
-      round: this.currentRound,
-      enemyHit: workflow.enemyHit,
-      attackTotal: 0,
-      damageTotal: 0,
-      damageMultipleEnemiesTotal: 0,
-      isCritical: false,
-    };
+    const isExistingEvent: boolean =
+      lastCombatantEvent !== undefined &&
+      lastCombatantEvent.damageTotal !== undefined &&
+      lastCombatantEvent.damageTotal === 0 &&
+      workflow.damageTotal > 0 &&
+      lastCombatantEvent.actorId === workflow.actor.id &&
+      lastCombatantEvent.round === this.currentRound &&
+      lastCombatantEvent.item?.id === workflow.item.id;
 
-    if (this.IsValidAttack(newCombatantEvent.actionType)) {
-      if (workflow.attackTotal) {
-        newCombatantEvent.attackTotal = workflow.attackTotal;
+    if (isExistingEvent) {
+      const newCombatantEvent =
+        combatantStat.events[combatantStat.events.length - 1];
+
+      newCombatantEvent.damageTotal = workflow.damageTotal;
+    } else {
+      const newCombatantEvent = <CombatantEvent>{
+        id: workflow.id,
+        actorId: workflow.actor.id,
+        item: workflow.item,
+        advantage: workflow.advantage,
+        disadvantage: workflow.disadvantage,
+        actionType: workflow.actionType,
+        round: this.currentRound,
+        enemyHit: workflow.enemyHit,
+        attackTotal: 0,
+        damageTotal: 0,
+        damageMultipleEnemiesTotal: 0,
+        isCritical: false,
+      };
+
+      if (this.IsValidAttack(newCombatantEvent.actionType)) {
+        if (workflow.attackTotal) {
+          newCombatantEvent.attackTotal = workflow.attackTotal;
+        }
       }
-    }
-    if (this.IsValidRollEvent(newCombatantEvent.actionType)) {
-      if (workflow.damageTotal) {
-        newCombatantEvent.damageMultipleEnemiesTotal =
-          workflow.damageMultipleEnemiesTotal;
-        newCombatantEvent.damageTotal = workflow.damageTotal;
-        newCombatantEvent.isCritical = workflow.isCritical;
+      if (this.IsValidRollEvent(newCombatantEvent.actionType)) {
+        if (workflow.damageTotal) {
+          newCombatantEvent.damageMultipleEnemiesTotal =
+            workflow.damageMultipleEnemiesTotal;
+          newCombatantEvent.damageTotal = workflow.damageTotal;
+          newCombatantEvent.isCritical = workflow.isCritical;
+        }
       }
+      combatantStat.events.push(newCombatantEvent);
     }
-    combatantStat.events.push(newCombatantEvent);
   }
 }
